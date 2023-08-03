68401e8604b0c896678201bbd12b92cf64970a83
AmanVerma5 <amanv4775@gmail.com>
2023-08-03 11:34:32 +0530

    submitting code-Thu Aug 03 2023 11:34:29 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
+import React from "react";
 
+const App=()=>{
+    return(
+        <div>
+            <div className="viewport">Pages Go Here</div>
+            <div className="tabs">
+                <div className="tab" id="Home-tab">Home</div>
+                <div className="tab" id="About-tab">About</div>
+                <div className="tab" id="Features-tab">Features</div>
+            </div>
+        </div>
+    )
+}
+
+export default App
